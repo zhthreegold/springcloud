bf89ca797f57c803fbb8baa216d818dd2e97fadb
Xin Zhuang <xin.zhuang@traderev.com>
2018-03-01 10:20:31 -0500

    initial commit

---
+++ greating/src/main/java/xin/test/greating/GreatingApplication.java
@@ -0,0 +1,12 @@
+package xin.test.greating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatingApplication.class, args);
+	}
+}
---
+++ greating/src/test/java/xin/test/greating/GreatingApplicationTests.java
@@ -0,0 +1,16 @@
+package xin.test.greating;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreatingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
